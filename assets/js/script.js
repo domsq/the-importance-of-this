f7926c0fae5805d49ebc71220175303f4df03a51
domsq <Domsq@hotmail.com>
2021-10-02 16:51:33 +0000

    Still need to complete box class exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-$('p').click(function(){
-    $(this).slideToggle('slow');    
-})
+$(document).ready(function(){
+    $(".box").click(function(){
+        let classNames = $(this).attr("class").split(" ");
+        let boxClass = classNames[0];
+        let className = classNames[1];
+        if($(this).css("background") === "rgb(255, 0, 0)" {
+            $("." + className).css("background", "#000");            
+        }
+        else {
+            $("." + boxClass).css("background", "#000");
+            $("." + className).css("background", "red");
+        }
+        )
+    )
+)
